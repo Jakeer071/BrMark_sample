95502cc879c21b37a2786383f54de768766b9533
Jakeer071 <101798080+Jakeer071@users.noreply.github.com>
2024-08-12 17:56:10 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FeaturesComponent } from './components/features/features.component';
 import { WeatherWidgetComponent } from './components/weather-widget/weather-widget.component';
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FeaturesComponent,
     WeatherWidgetComponent
   ],
   imports: [
